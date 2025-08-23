7bd0bf6ce896b58fc5fa9ec70de0e05effda6229
Heesho <hisham11027@gmail.com>
2025-08-23 17:18:38 -0400

    deployed failed, fixes, trying deployment again

--- app/components/ui/Icon/Icon.types.ts
+++ app/components/ui/Icon/Icon.types.ts
@@ -1,8 +1,21 @@
 export interface IconProps {
-  name: "heart" | "star" | "check" | "plus" | "arrow-right" | "home" | "search" | "create" | "notifications" | "profile" | "message" | "warning";
-  size?: "sm" | "md" | "lg";
+  name: IconName;
+  size?: IconSize;
   className?: string;
 }
 
-export type IconName = "heart" | "star" | "check" | "plus" | "arrow-right" | "home" | "search" | "create" | "notifications" | "profile" | "message" | "warning";
+export type IconName =
+  | "heart"
+  | "star"
+  | "check"
+  | "plus"
+  | "arrow-right"
+  | "home"
+  | "search"
+  | "create"
+  | "notifications"
+  | "profile"
+  | "message"
+  | "warning"
+  | "wallet";
 export type IconSize = "sm" | "md" | "lg";
